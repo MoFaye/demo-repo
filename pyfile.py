099d8c0f8ffd5c45be6998578116405e8b0c07d9
Mohammad Alfayez <mohdhf95@gmail.com>
2023-10-08 21:58:27 +0300

    fff git push

--- pyfile.py
+++ pyfile.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 
-def add_nums(a, b):
-    return a + b
+class adding_nums:
+    def __init__(self, a, b):
+        self.first_num = a
+        self.second_num = b
 
-print(add_nums(4,3))
+    def add(self):
+        self.add = self.first_num + self.second_num
+        print(self.add)
+
+
+if __name__ == "__main__":
+    f = adding_nums(5,6)
+    f.add()
